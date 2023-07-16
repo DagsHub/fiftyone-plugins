fce62c8a415d607ff13a808fcb30ec6de6337a96
KBolashev <kirill@dagshub.com>
2023-07-16 10:48:36 +0300

    Update copy-to-client.js to copy the icon

--- packages/dagshub/copy-to-client.js
+++ packages/dagshub/copy-to-client.js
@@ -8,8 +8,9 @@ function getOutputFolder() {
   }
 
   // Get from env var
-  const env_path = process.env.DAGSHUB_PLUGIN_PATH
+  let env_path = process.env.DAGSHUB_FIFTYONE_PLUGINS_DIR
   if (env_path !== undefined) {
+    env_path = path.join(env_path, "dagshub");
     return env_path;
   }
 
@@ -18,11 +19,16 @@ function getOutputFolder() {
 
 function copyBuiltFiles(outputDir) {
   const dist_path = path.join(outputDir, "dist");
+  const asset_path = path.join(outputDir, "assets");
   if (!fs.existsSync(dist_path)) {
     fs.mkdirSync(dist_path, {recursive: true});
   }
+  if (!fs.existsSync(asset_path)) {
+    fs.mkdirSync(asset_path, {recursive: true});
+  }
 
   fs.cpSync(path.join(__dirname, "dist", "index.umd.js"), path.join(dist_path, "index.umd.js"));
+  fs.cpSync(path.join(__dirname, "assets", "dagshub.svg"), path.join(asset_path, "dagshub.svg"));
   fs.cpSync(path.join(__dirname, "_deploypackage.json"), path.join(outputDir, "package.json"));
   fs.cpSync(path.join(__dirname, "fiftyone.yaml"), path.join(outputDir, "fiftyone.yaml"));
 }
